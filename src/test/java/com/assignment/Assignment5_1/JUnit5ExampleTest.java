85ce8c79e1c18bb8674e2466f39bcec9a64b6e5b
Kumar, Vijay (vk3768) <vk3768@att.com>
2018-11-26 17:26:07 +0530

    initial commit

---
+++ src/test/java/com/assignment/Assignment5_1/JUnit5ExampleTest.java
@@ -0,0 +1,60 @@
+package com.assignment.Assignment5_1;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import org.junit.Assert;
+ 
+class JUnit5ExampleTest {
+ 
+    @Test
+    void justAnExample() {
+        System.out.println("This test method should be run");
+    }
+    @Test
+    public void testAppSuccess()
+    {
+    	System.setProperty("webdriver.chrome.driver", "C:\\vijay\\Training\\edureka\\Assignments\\Assignment.5\\chromedriver.exe");
+    	//System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+    		
+		ChromeOptions chromeOptions = new ChromeOptions();
+		chromeOptions.setHeadless(true);
+		WebDriver driver = new ChromeDriver(chromeOptions);
+		System.out.println("Completing Assignment 5");
+		driver.get("http://13.233.172.4:3001/");
+		String x = driver.getTitle();
+	    System.out.println(x);
+		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+		driver.findElement(By.name("login")).sendKeys("user1");
+		driver.findElement(By.name("password")).sendKeys("user1");
+		driver.findElement(By.name("click")).click(); 
+		System.out.println(driver.findElement(By.name("hello")).getText());
+		Assert.assertTrue(driver.findElement(By.name("hello")).getText().contains("Login Succeeded"));
+		driver.quit();
+    }
+    
+    @Test
+    public void testAppFail()
+    {
+    	System.setProperty("webdriver.chrome.driver", "C:\\vijay\\Training\\edureka\\Assignments\\Assignment.5\\chromedriver.exe");
+		//System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+		ChromeOptions chromeOptions = new ChromeOptions();
+		chromeOptions.setHeadless(true);
+		WebDriver driver = new ChromeDriver(chromeOptions);
+		System.out.println("Completing Assignment 5");
+		driver.get("http://13.233.172.4:3001/");
+		String x = driver.getTitle();
+	    System.out.println(x);
+		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+		driver.findElement(By.name("login")).sendKeys("user1");
+		driver.findElement(By.name("password")).sendKeys("user2");
+		driver.findElement(By.name("click")).click(); 
+		System.out.println(driver.findElement(By.name("hello")).getText());
+		Assert.assertTrue(driver.findElement(By.name("hello")).getText().contains("Login Failed!"));
+		driver.quit();
+    }
+}
